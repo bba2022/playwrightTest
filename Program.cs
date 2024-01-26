a1ed5524329f2f33b01bd42354c54fb89d794af0
20230704 <20230704@qq.com>
2024-01-27 01:08:18 +0800

    1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace playwrightTest
 {
     internal class Program
     {
-        public static ConcurrentDictionary<string, WebsiteUser> websiteUsers = new ConcurrentDictionary<string, WebsiteUser>();
+       
         static void Main(string[] args)
         {
             _ = testAsync();
@@ -15,16 +15,15 @@ namespace playwrightTest
         public static async Task testAsync() {
             try
             {
-                websiteUsers["1"] = new WebsiteUser();
                 var playwright = await Playwright.CreateAsync();
                 BrowserTypeLaunchPersistentContextOptions options = new BrowserTypeLaunchPersistentContextOptions();
-                options.ExecutablePath = "C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe";
+                options.ExecutablePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
                 options.Headless = false;
                 var browser = await playwright.Chromium.LaunchPersistentContextAsync("D:\\opt\\1", options);
-                await new PlaywrightFilter(websiteUsers["1"]).filterAsync(browser);
                  var page = await browser.NewPageAsync();
                 await page.AddInitScriptAsync("setInterval(function() {window.location.reload(true);}, 1000); ");
-                await page.GotoAsync("D:\\opt\\mobilewebdriver.js");
+  
+                _= page.GotoAsync(Directory.GetCurrentDirectory()+"\\test.js");
             }
             catch (Exception ee) {
                 Console.WriteLine(ee);
